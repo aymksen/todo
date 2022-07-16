84d5e36022d2de31026b1d4aa7302f36d779ace4
Aymen <76121581+aymks@users.noreply.github.com>
2022-07-16 01:28:27 +0100

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import React from 'react';
 import './App.css';
-import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="App">
+    <div className='todo-app'>
       <TodoList />
     </div>
   );
